4f2e8a16ef5334445777eb847dfd4885e5fb7c4a
weijian <chen_wei_jian5566@hotmail.com>
2018-10-10 11:04:47 +0800

    added item retrieval

--- codeitsuisse/routes/hardcode.py
+++ codeitsuisse/routes/hardcode.py
@@ -6,6 +6,7 @@ from codeitsuisse import app
 
 # logger = logging.getLogger(__name__)
 
+
 @app.route('/hardcode', methods=['GET'])
 def hardcode():
     # data = request.get_json()
@@ -15,26 +16,26 @@ def hardcode():
     # app.logger.info("My result :{}".format(result))
 
     response = {
-        "items":[
+        "items": [
             {
-                "id":1,
-                "name":"jelly",
-                "qty":2,
+                "id": 1,
+                "name": "jelly",
+                "qty": 2,
                 "price": 9.54
-            },{
-                "id":1,
-                "name":"peanut",
-                "qty":1,
+            }, {
+                "id": 2,
+                "name": "peanut",
+                "qty": 1,
                 "price": 15.24
-            },{
-                "id":1,
-                "name":"shampoo",
-                "qty":3,
+            }, {
+                "id": 3,
+                "name": "shampoo",
+                "qty": 3,
                 "price": 1.00
-            },{
-                "id":1,
-                "name":"spam",
-                "qty":1,
+            }, {
+                "id": 4,
+                "name": "spam",
+                "qty": 1,
                 "price": 0.25
             }
         ]
@@ -42,3 +43,36 @@ def hardcode():
     return jsonify(response)
     # return jsonify(result)
 
+
+@app.route('/item/<item_id>', methods=['GET'])
+def get_item(item_id):
+
+    items = {
+        '0001': {
+            "id": 1,
+            "name": "jelly",
+            "qty": 2,
+            "price": 9.54
+        },
+        '0002': {
+            "id": 2,
+            "name": "peanut",
+            "qty": 1,
+            "price": 15.24
+        },
+        '0003': {
+            "id": 3,
+            "name": "shampoo",
+            "qty": 3,
+            "price": 1.00
+        },
+        '0004': {
+            "id": 4,
+            "name": "spam",
+            "qty": 1,
+            "price": 0.25
+        }
+    }
+
+    response = items[item_id]
+    return jsonify(response)
